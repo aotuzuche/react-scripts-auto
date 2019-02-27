68a32ba64f09fd8c6f28947a6fbe0449d5c3290e
Eason chiu <76670758@qq.com>
2019-02-27 12:01:05 +0800

    增加使用备注

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -23,7 +23,12 @@ const app = dva({
 });
 
 if (process.env.REACT_APP_PACKAGE === 'dev') {
-  console.log('这是一段只会在dev环境打印的话，如果要做一些环境区分，请使用该方式');
+  console.warn(
+    '\n\n这是一段只会在dev环境打印的话，如果要做一些环境区分，请使用该方式\n注意：\nREACT_APP_PACKAGE = dev 时，代表环境为本地或测试1~5\n若只判断本地，请使用 process.env.NODE_ENV = development ，该变量在测试1~5或正式环境时 = production\n\n',
+  );
+  console.warn(
+    '\n\n其他注意事项：\nbasename: 需要修改两处，src/index.tsx和根目录下的.env文件\n预登录: 在src/index.tsx中处理，默认需要登录\nproxy: 在package.json中增加"proxy": "https://test1-web.autozuche.com"\n\nauto-ui地址: https://aotuzuche.github.io/auto-ui/\n\n',
+  );
 }
 
 app.model(demoModel);
