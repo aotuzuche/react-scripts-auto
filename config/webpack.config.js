455ef20c84ca7b6f5b3dbed56f7a7ad15c6c7674
Eason chiu <76670758@qq.com>
2019-04-29 17:34:18 +0800

    set runtimeChunk to false

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -258,7 +258,7 @@ module.exports = function(webpackEnv) {
       },
       // Keep the runtime chunk separated to enable long term caching
       // https://twitter.com/wSokra/status/969679223278505985
-      runtimeChunk: true,
+      runtimeChunk: false,
     },
     resolve: {
       // This allows you to set a fallback for where Webpack should look for modules.
