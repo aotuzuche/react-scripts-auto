1f1e789415f96506d6fa470355dc516df5f189c6
Eason chiu <76670758@qq.com>
2019-09-12 15:09:18 +0800

    修改构建命令

--- bin/before-build.sh
+++ bin/before-build.sh
@@ -2,4 +2,5 @@
 
 node node_modules/react-scripts-auto/bin/check-version.js
 node node_modules/react-scripts-auto/bin/rewrite-env.js $1
+node node_modules/react-scripts-auto/bin/save-branch.js $1
 node node_modules/react-scripts-auto/bin/set-slot-content.js $1
--- bin/before-dev.sh
+++ bin/before-dev.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 node node_modules/react-scripts-auto/bin/check-version.js
+node node_modules/react-scripts-auto/bin/save-branch.js test
 node node_modules/react-scripts-auto/bin/set-slot-content.js test
