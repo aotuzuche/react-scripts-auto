67e937b102d2fcb57c8e14a00cf304be8e67ab8b
Eason chiu <76670758@qq.com>
2019-09-17 16:10:31 +0800

    构建时生成prettierrc文件

--- bin/before-dev.sh
+++ bin/before-dev.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 git fetch
+node node_modules/react-scripts-auto/bin/create-prettierrc.js
 node node_modules/react-scripts-auto/bin/check-version.js
 node node_modules/react-scripts-auto/bin/save-branch.js test
 node node_modules/react-scripts-auto/bin/set-slot-content.js test
