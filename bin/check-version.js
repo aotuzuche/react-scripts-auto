5905e1e129018ce9545dccc328987f13b178a74c
Eason chiu <76670758@qq.com>
2019-10-15 15:17:51 +0800

    添加doctor

--- bin/check-version.js
+++ bin/check-version.js
@@ -1,40 +1,9 @@
 /**
   检测依赖的版本
  */
-const fs = require('fs')
-const fetch = require('node-fetch')
-
-function fetchRepoVersion(name) {
-  return new Promise((resolve) => {
-    fetch(`https://registry.npm.taobao.org/${name}/latest`)
-      .then((res) => res.json())
-      .then((json) => resolve(json.version))
-      .catch(() => resolve(''))
-  })
-}
-
-let packageCache
-
-function fetchLocalPackageVersion(name) {
-  if (__dirname.indexOf('node_modules') === -1) {
-    return '[未安装]'
-  }
-  let json
-  if (packageCache) {
-    json = packageCache
-  } else {
-    const projectPath = __dirname.split('node_modules')[0]
-    const file = fs.readFileSync(`${projectPath}package.json`)
-    json = JSON.parse(String(file) || '{}')
-  }
-
-  if (json.dependencies && json.dependencies[name]) {
-    return json.dependencies[name].replace('^', '')
-  } else if (json.devDependencies && json.devDependencies[name]) {
-    return json.devDependencies[name].replace('^', '')
-  }
-  return '[未安装]'
-}
+const version = require('./version')
+const fetchRepoVersion = version.fetchRepoVersion
+const fetchLocalPackageVersion = version.fetchLocalPackageVersion
 
 Promise.all([
   fetchRepoVersion('auto-libs'),
@@ -45,37 +14,45 @@ Promise.all([
   fetchLocalPackageVersion('react-scripts-auto'),
   fetchRepoVersion('eslint-config-atzuche'),
   fetchLocalPackageVersion('eslint-config-atzuche'),
+  fetchRepoVersion('vconsole-atzuche-env'),
+  fetchLocalPackageVersion('vconsole-atzuche-env'),
+  fetchRepoVersion('vconsole-atzuche-webpack-plugin'),
+  fetchLocalPackageVersion('vconsole-atzuche-webpack-plugin'),
 ]).then((res) => {
   if (res[1] !== res[0]) {
-    console.log('')
-    console.log(`    auto-libs 包不是最新版本，当前版本为 ${res[1]}，请升级至 ${res[0]}`)
-    console.log('    yarn add auto-libs')
-    console.log('')
+    console.log(`[WRONG] auto-libs 包不是最新版本，当前版本为 ${res[1]}，请升级至 ${res[0]}`)
+    console.log('        yarn add auto-libs')
   } else {
     console.log(`[OK] auto-libs 是最新版本`)
   }
   if (res[3] !== res[2]) {
-    console.log('')
-    console.log(`    auto-ui 包不是最新版本，当前版本为 ${res[3]}，请升级至 ${res[2]}`)
-    console.log('    yarn add auto-ui')
-    console.log('')
+    console.log(`[WRONG] auto-ui 包不是最新版本，当前版本为 ${res[3]}，请升级至 ${res[2]}`)
+    console.log('        yarn add auto-ui')
   } else {
     console.log(`[OK] auto-ui 是最新版本`)
   }
   if (res[5] !== res[4]) {
-    console.log('')
-    console.log(`    react-scripts-auto 包不是最新版本，当前版本为 ${res[5]}，请升级至 ${res[4]}`)
-    console.log('    yarn add react-scripts-auto')
-    console.log('')
+    console.log(`[WRONG] react-scripts-auto 包不是最新版本，当前版本为 ${res[5]}，请升级至 ${res[4]}`)
+    console.log('        yarn add react-scripts-auto')
   } else {
     console.log(`[OK] react-scripts-auto 是最新版本`)
   }
   if (res[7] !== res[6]) {
-    console.log('')
-    console.log(`    eslint-config-atzuche 包不是最新版本，当前版本为 ${res[7]}，请升级至 ${res[6]}`)
-    console.log('    yarn add eslint-config-atzuche -D')
-    console.log('')
+    console.log(`[WRONG] eslint-config-atzuche 包不是最新版本，当前版本为 ${res[7]}，请升级至 ${res[6]}`)
+    console.log('        yarn add eslint-config-atzuche -D')
   } else {
     console.log(`[OK] eslint-config-atzuche 是最新版本`)
+  }
+  if (res[9] !== res[8]) {
+    console.log(`[WRONG] vconsole-atzuche-env 包不是最新版本，当前版本为 ${res[7]}，请升级至 ${res[6]}`)
+    console.log('        yarn add vconsole-atzuche-env -D')
+  } else {
+    console.log(`[OK] vconsole-atzuche-env 是最新版本`)
+  }
+  if (res[11] !== res[10]) {
+    console.log(`[WRONG] vconsole-atzuche-webpack-plugin 包不是最新版本，当前版本为 ${res[7]}，请升级至 ${res[6]}`)
+    console.log('        yarn add vconsole-atzuche-webpack-plugin -D')
+  } else {
+    console.log(`[OK] vconsole-atzuche-webpack-plugin 是最新版本`)
   }
 })
---
+++ bin/doctor.js
@@ -0,0 +1,184 @@
+const fs = require('fs')
+const path = require('path')
+const crypto = require('crypto')
+const version = require('./version')
+const shell = require('shelljs')
+
+// 检查更新eslintrc.js
+// 检查并更新bin下的所有文件
+// 检查package.json
+// 1. husky、lint-staged配置
+// 2. auto-libs auto-ui react-scripts-auto at-js-sdk eslint-config-atzuche vconsole vconsole-atzuche-env vconsole-atzuche-webpack-plugin 最新
+// 3. fastclick
+// 4. husky lint-staged prettier eslint-plugin-react 安装
+
+// 如果没找到node_modules目录，结束
+if (__dirname.indexOf('node_modules') === -1) {
+  return
+}
+
+const projectPath = __dirname.split('node_modules')[0]
+const templatePath = path.join(__dirname, '../template')
+
+// 检查脚手架是否为最新版本
+function checkRSAVersion() {
+  return new Promise((resolve, reject) => {
+    Promise.all([
+      version.fetchRepoVersion('react-scripts-auto'),
+      version.fetchLocalPackageVersion('react-scripts-auto'),
+    ]).then(res => {
+        if (res[1] === res[0]) {
+          console.log('[OK] react-scripts-auto 是最新版本')
+          resolve()
+        } else {
+          console.log(`[WRONG] react-scripts-auto 包不是最新版本，当前版本为 ${res[1]}，请升级至 ${res[0]}`)
+          console.log('        需要安装最新的')
+          console.log('        yarn add react-scripts-auto')
+          reject()
+        }
+    })
+  })
+}
+
+// 主要依赖是否为最新
+function checkImportantPackageVersion() {
+  return new Promise((resolve) => {
+    Promise.all([
+      version.fetchRepoVersion('auto-libs'),
+      version.fetchLocalPackageVersion('auto-libs'),
+      version.fetchRepoVersion('auto-ui'),
+      version.fetchLocalPackageVersion('auto-ui'),
+      version.fetchRepoVersion('eslint-config-atzuche'),
+      version.fetchLocalPackageVersion('eslint-config-atzuche'),
+      version.fetchRepoVersion('vconsole-atzuche-env'),
+      version.fetchLocalPackageVersion('vconsole-atzuche-env'),
+      version.fetchRepoVersion('vconsole-atzuche-webpack-plugin'),
+      version.fetchLocalPackageVersion('vconsole-atzuche-webpack-plugin'),
+    ]).then(res => {
+        if (res[1] !== res[0]) {
+          console.log(`[WRONG] auto-libs 包不是最新版本，当前版本为 ${res[1]}，请升级至 ${res[0]}`)
+          console.log('        yarn add auto-libs')
+        } else {
+          console.log(`[OK] auto-libs 是最新版本`)
+        }
+        if (res[3] !== res[2]) {
+          console.log(`[WRONG]  auto-ui 包不是最新版本，当前版本为 ${res[3]}，请升级至 ${res[2]}`)
+          console.log('         yarn add auto-ui')
+        } else {
+          console.log(`[OK] auto-ui 是最新版本`)
+        }
+        if (res[5] !== res[4]) {
+          console.log(`[WRONG] eslint-config-atzuche 包不是最新版本，当前版本为 ${res[7]}，请升级至 ${res[6]}`)
+          console.log('        yarn add eslint-config-atzuche -D')
+        } else {
+          console.log(`[OK] eslint-config-atzuche 是最新版本`)
+        }
+        if (res[7] !== res[6]) {
+          console.log(`[WRONG] vconsole-atzuche-env 包不是最新版本，当前版本为 ${res[7]}，请升级至 ${res[6]}`)
+          console.log('        yarn add vconsole-atzuche-env -D')
+        } else {
+          console.log(`[OK] vconsole-atzuche-env 是最新版本`)
+        }
+        if (res[9] !== res[8]) {
+          console.log(`[WRONG] vconsole-atzuche-webpack-plugin 包不是最新版本，当前版本为 ${res[7]}，请升级至 ${res[6]}`)
+          console.log('        yarn add vconsole-atzuche-webpack-plugin -D')
+        } else {
+          console.log(`[OK] vconsole-atzuche-webpack-plugin 是最新版本`)
+        }
+
+        resolve()
+    })
+  })
+}
+
+// 检查更新eslintrc.js
+function checkEslint() {
+  return new Promise(resolve => {
+    const exists = fs.existsSync(path.join(projectPath, '.eslintrc.js'))
+    if (exists) {
+      const f1 = String(fs.readFileSync(path.join(projectPath, '.eslintrc.js')))
+      const f2 = String(fs.readFileSync(path.join(templatePath, '.eslintrc.js')))
+      if (f1 !== f2) {
+        fs.copyFileSync(path.join(templatePath, '.eslintrc.js'), path.join(projectPath, '.eslintrc.js'))
+        console.log('[WRONG] 不允许私自修改.eslintrc.js文件，自动复原')
+      } else {
+        console.log('[OK] .eslintrc.js文件正常')
+      }
+    } else {
+      fs.copyFileSync(path.join(templatePath, '.eslintrc.js'), path.join(projectPath, '.eslintrc.js'))
+      console.log('[WRONG] 缺少.eslintrc.js文件，自动创建')
+    }
+
+    resolve()
+  })
+}
+
+// 检查并更新bin下的所有文件
+function checkBinFiles() {
+  return new Promise(resolve => {
+    fs.copyFileSync(
+      path.join(templatePath, 'bin', 'dev.sh'),
+      path.join(projectPath, 'bin', 'dev.sh'),
+    )
+    fs.copyFileSync(
+      path.join(templatePath, 'bin', 'prod.sh'),
+      path.join(projectPath, 'bin', 'prod.sh'),
+    )
+    fs.copyFileSync(
+      path.join(templatePath, 'bin', 'test.sh'),
+      path.join(projectPath, 'bin', 'test.sh'),
+    )
+
+    resolve()
+  })
+}
+
+// 检查package.json husky、lint-staged配置
+function checkPackageJson() {
+  return new Promise(resolve => {
+    let f = String(fs.readFileSync(path.join(projectPath, 'package.json')))
+    const jsonF = JSON.parse(f)
+
+    if (!jsonF.scripts.doctor) {
+      f = f.replace(/"__build".+,/, (i) => {
+        return i + '\r\t\t"doctor": "node ./node_modules/react-scripts-auto/bin/doctor.js",'
+      })
+      fs.writeFileSync(path.join(projectPath, 'package.json'), f)
+    }
+
+    if (f.indexOf('fastclick') !== -1) {
+      console.log('[WRONG] 请勿使用 fastclick 库，自动删除')
+      shell.exec('yarn remove fastclick')
+    }
+    if (f.indexOf('husky') === -1) {
+      console.log('[WRONG] 缺少 husky 库，自动安装')
+      shell.exec('yarn add husky -D')
+    }
+    if (f.indexOf('lint-staged') === -1) {
+      console.log('[WRONG] 缺少 lint-staged 库，自动安装')
+      shell.exec('yarn add lint-staged -D')
+    }
+    if (f.indexOf('prettier') === -1) {
+      console.log('[WRONG] 缺少 prettier 库，自动安装')
+      shell.exec('yarn add prettier -D')
+    }
+    if (f.indexOf('eslint-plugin-react') === -1) {
+      console.log('[WRONG] 缺少 eslint-plugin-react 库，自动安装')
+      shell.exec('yarn add eslint-plugin-react -D')
+    }
+
+    resolve()
+  })
+}
+
+checkRSAVersion()
+  .then(checkImportantPackageVersion)
+  .then(checkEslint)
+  .then(checkBinFiles)
+  .then(checkPackageJson)
+  .then(() => {
+    console.log('')
+    console.log('     doctor检查完毕')
+    console.log('')
+  })
+  .catch(() => {})
